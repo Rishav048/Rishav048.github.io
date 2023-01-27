ad112f15ee48316b7429a295cd9b14b2defaa24b
Rishav048 <chakrabortyrishav048@gmail.com>
2023-01-27 15:43:17 +0530

    Profolio Final Touch1

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <Box bg={"#2a2b2b"} w={"100%"} h={"170px"} border={"2px solid black"} p={6}>
       <Heading className={style.Thankyou} fontFamily={"Fugaz One, cursive"}>
-        Thank you for visiting.😊
+        Thank you for visiting.😀
       </Heading>
       <Text fontWeight={"500"} textAlign={"center"} color={"white"}>
         © 2023 Designed and Built by Rishav Chakraborty
